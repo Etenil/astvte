cbaee48adc53e9865e7ce7794b42073a4033d485
Guillaume Pasquet <boss@etenil.net>
2013-02-04 17:10:20 +0800

    Improved excerpts.

--- models/utils.php
+++ models/utils.php
@@ -4,6 +4,6 @@ class Model_Utils extends \assegai\Model
 {
     function excerpt($text)
     {
-        return substr($text, 0, min(strlen($text), 200));
+        return substr($text, 0, min(strlen($text), 200)) . '...';
     }
 }
